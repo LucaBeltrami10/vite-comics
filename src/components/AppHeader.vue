7bbbb8291c4f580be75e9f1ce968a66f2d5352b3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-14 18:02:58 +0100

    eliminato component e aggiunto nav a appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,23 @@
 <template>
     <header>
         <div class="container">
-            ciaone
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="Logo DC">
+            </div>
+            <nav>
+                <ul>
+                    <li>characters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibiles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+            </nav>
         </div>
     </header>
 </template>
@@ -18,5 +34,13 @@ export default {
 
 div.container {
     background-color: $header-background;
+
+    div {
+        height: 3.5rem;
+
+        img {
+            height: 100%;
+        }
+    }
 }
 </style>
