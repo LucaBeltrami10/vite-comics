702c5cac73326e4ce3b6b11e968fa77848f6709a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-14 17:23:58 +0100

    prova su appheader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    components: {
+        AppHeader,
+    }
+}
 
 </script>
 
-<template></template>
+<template>
+    <AppHeader />
+</template>
 
 <style scoped></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,22 @@
 <template>
     <header>
-        <div>
-
+        <div class="container">
+            ciaone
         </div>
     </header>
 </template>
 
 <script>
 export default {
+    name: 'AppHeader',
 
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+div.container {
+    width: 70%;
+    background-color: aqua;
+    margin: 0 auto;
+}
+</style>
