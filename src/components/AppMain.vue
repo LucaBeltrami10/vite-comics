da661bf4e3027b3a2129ad10b5d339788ecf3138
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-15 16:01:56 +0100

    inserito cont component app main e inserito in app.vue

---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<template>
+    <main>
+        <div class="container">
+            <p>--- CONTENUTO ---</p>
+        </div>
+
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+main {
+    color: $main-background;
+
+    p {
+        padding: 1rem 0;
+    }
+}
+</style>
