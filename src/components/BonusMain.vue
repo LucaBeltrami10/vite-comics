fd6a430d436df7d5a9dd101d92d31db2d3888697
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-18 09:16:36 +0100

    inserito contenuto BonusMain

--- src/components/BonusMain.vue
+++ src/components/BonusMain.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
+    <div class="container">
+        <article>
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <p>digital comics</p>
+        </article>
+        <article>
+            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+            <p>dc merchandise</p>
+        </article>
+        <article>
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <p>subscription</p>
+        </article>
+        <article>
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <p>comic shop locator</p>
+        </article>
+        <article>
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <p>dc power visa</p>
+        </article>
 
     </div>
 </template>
+
 <script>
 export default {
 
 }
 </script>
+
 <style lang="scss" scoped></style>
