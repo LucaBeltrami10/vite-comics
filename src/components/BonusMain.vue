0df14a930e8f89bb1d3c9f7629ca42a8cc961cee
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-18 09:29:07 +0100

    inserito bonus

--- src/components/BonusMain.vue
+++ src/components/BonusMain.vue
@@ -1,25 +1,28 @@
 <template>
-    <div class="container">
-        <article>
-            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-            <p>digital comics</p>
-        </article>
-        <article>
-            <img src="../assets/img/buy-comics-merchandise.png" alt="">
-            <p>dc merchandise</p>
-        </article>
-        <article>
-            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-            <p>subscription</p>
-        </article>
-        <article>
-            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-            <p>comic shop locator</p>
-        </article>
-        <article>
-            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-            <p>dc power visa</p>
-        </article>
+    <div>
+        <div class="container">
+            <article>
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <p>digital comics</p>
+            </article>
+            <article>
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <p>dc merchandise</p>
+            </article>
+            <article>
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <p>subscription</p>
+            </article>
+            <article>
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <p>comic shop locator</p>
+            </article>
+            <article>
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <p>dc power visa</p>
+            </article>
+
+        </div>
 
     </div>
 </template>
@@ -35,22 +38,29 @@ export default {
 @use '../styles/partials/mixins' as *;
 @use '../styles/partials/variables' as *;
 
-div.container {
-    @include flex(row, space-between, center);
+div {
     background-color: $background-bonus;
-    color: $color-bonus;
 
-    article {
+    div.container {
         @include flex(row, space-between, center);
-        padding: 1.5rem 0;
 
-        img {
-            height: 40px;
-        }
+        color: $color-bonus;
 
-        p {
-            margin-left: 0.5rem;
+        article {
+            @include flex(row, space-between, center);
+            padding: 2.5rem 0;
+
+            img {
+                height: 40px;
+            }
+
+            p {
+                margin-left: 0.5rem;
+                text-transform: uppercase;
+                font-size: 0.8rem;
+            }
         }
     }
+
 }
 </style>
